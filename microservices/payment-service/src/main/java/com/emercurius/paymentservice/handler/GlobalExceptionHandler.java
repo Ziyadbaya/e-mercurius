95b071f734af8df2fc635f86527a7ba243f3d933
ziyad baya <baya.ziyad.mp@gmail.com>
2025-08-12 01:48:25 +0100

    Add NoHandlerFoundException and NoResourceFoundException handler

--- microservices/payment-service/src/main/java/com/emercurius/paymentservice/handler/GlobalExceptionHandler.java
+++ microservices/payment-service/src/main/java/com/emercurius/paymentservice/handler/GlobalExceptionHandler.java
@@ -6,15 +6,25 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.NoHandlerFoundException;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(EntityNotFoundException.class)
-    public ResponseEntity<ErrorResponse> handle(EntityNotFoundException ex) {
+    public ResponseEntity<ErrorResponse> handleEntityNotFoundException(EntityNotFoundException ex) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND)
                 .body(
                         new ErrorResponse(ex.getMessage(), HttpStatus.NOT_FOUND.value())
+                );
+    }
+
+    @ExceptionHandler({NoHandlerFoundException.class, NoResourceFoundException.class})
+    public ResponseEntity<ErrorResponse> handle() {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(
+                        new ErrorResponse("Path not found", HttpStatus.NOT_FOUND.value())
                 );
     }
 }
